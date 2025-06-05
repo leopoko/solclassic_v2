09f17d46a828581e6705de367f3356404dd8228f
leopoko <32629847+leopoko@users.noreply.github.com>
2025-06-05 13:40:38 +0900

    Update default blacklist item

--- common/src/main/java/com/github/leopoko/solclassic/config/SolclassicConfigData.java
+++ common/src/main/java/com/github/leopoko/solclassic/config/SolclassicConfigData.java
@@ -8,6 +8,7 @@ public class SolclassicConfigData {
     public static int maxShortFoodHistorySize = 5;
     public static float longFoodDecayModifiers = 0.01F;
     public static List<Float> shortFoodDecayModifiers = Arrays.asList(1.0F, 0.9F, 0.75F, 0.5F, 0.05F);
-    public static List<String> foodBlacklist = Arrays.asList("minecraft:kelp");
+    // Items that will not be affected by food history tracking
+    public static List<String> foodBlacklist = Arrays.asList("minecraft:dried_kelp");
     public static boolean enableWickerBasket = true;
 }
